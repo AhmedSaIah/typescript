8833b7d54d6e77682f4a320ae60b80d54e40105c
AhmedSaIah <ahmedsalah5446@gmail.com>
2024-02-25 11:26:53 +0200

    secret changed

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,10 @@
 import crypto from "crypto";
-const SECRET = "rox";
+require('dotenv').config();
 
 export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (salt: string, password: string) => {
   return crypto
     .createHmac("sha256", [salt, password].join("/"))
-    .update(SECRET)
+    .update(process.env.CRYPT_SECRET)
     .digest("hex");
 };
