9d3151a9066ed7981af3cf8f2cd00811ab936835
hamdyyemadd <hamdyyemad@aol.com>
2024-09-04 18:06:35 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,36 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import router from './router';
-require('dotenv').config();
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-}));
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(process.env.PORT || 8080, () => {
-    console.log(`API running on http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`API running on http://localhost:${process.env.PORT}`);
 });
 
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGO_URL);
-mongoose.connection.on('error', (error: Error) => console.log(error));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("connected"))
+  .catch((err) => console.log(err));
+mongoose.connection.on("error", (error: Error) => console.log(error));
 
-app.use('/', router());
+app.use("/", router());
